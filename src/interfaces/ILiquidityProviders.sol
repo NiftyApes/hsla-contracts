9c67e3c61b88cd65b9f897d62400c3d2e828607a
Daniel Kurka <kurka.daniel@gmail.com>
2022-03-09 13:01:07 -0800

    Remove unused imports.

--- src/interfaces/ILiquidityProviders.sol
+++ src/interfaces/ILiquidityProviders.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import "../ErrorReporter.sol";
-import "../CarefulMath.sol";
 import "./ILiquidityProviderEvents.sol";
 
 // @dev public interface for LiquidityProviders.sol
