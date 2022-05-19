e7aeb5655d45269b9a9bcaa2edc5847f7352d5b5
captnseagraves <k.s.seagraves@gmail.com>
2022-05-18 20:35:03 -0400

    remove liquidity from lending

--- src/Lending.sol
+++ src/Lending.sol
@@ -14,7 +14,7 @@ import "@openzeppelin/contracts/utils/math/SafeCastUpgradeable.sol";
 import "@openzeppelin/contracts/utils/AddressUpgradeable.sol";
 import "./interfaces/compound/ICEther.sol";
 import "./interfaces/compound/ICERC20.sol";
-import "./interfaces/niftyapes/INiftyApes.sol";
+import "./interfaces/niftyapes/lending/ILending.sol";
 import "./interfaces/niftyapes/offers/IOffers.sol";
 import "./interfaces/sanctions/SanctionsList.sol";
 import "./lib/ECDSABridge.sol";
@@ -23,13 +23,13 @@ import "./lib/Math.sol";
 // import "./test/Console.sol";
 
 /// @title Implemention of the INiftyApes interface
-contract NiftyApes is
+contract NiftyApesLending is
     OwnableUpgradeable,
     PausableUpgradeable,
     ReentrancyGuardUpgradeable,
     EIP712Upgradeable,
     ERC721HolderUpgradeable,
-    INiftyApes
+    ILending
 {
     using SafeERC20Upgradeable for IERC20Upgradeable;
     using AddressUpgradeable for address payable;
@@ -47,18 +47,6 @@ contract NiftyApes is
     /// @notice The base value for fees in the protocol.
     uint256 private constant MAX_BPS = 10_000;
 
-    /// @inheritdoc ILiquidity
-    mapping(address => address) public override assetToCAsset;
-
-    /// @notice The reverse mapping for assetToCAsset
-    mapping(address => address) internal _cAssetToAsset;
-
-    /// @notice The account balance for each asset of a user
-    mapping(address => mapping(address => Balance)) internal _balanceByAccountByAsset;
-
-    /// @inheritdoc ILiquidity
-    mapping(address => uint256) public override maxBalanceByCAsset;
-
     /// @dev A mapping for a NFT to a loan auction.
     ///      The mapping has to be broken into two parts since an NFT is denomiated by its address (first part)
     ///      and its nftId (second part) in our code base.
@@ -73,15 +61,6 @@ contract NiftyApes is
     /// @inheritdoc ILending
     uint16 public refinancePremiumProtocolBps;
 
-    /// @inheritdoc ILending
-    uint16 public regenCollectiveBpsOfRevenue;
-
-    /// @dev @inheritdoc ILending
-    address public regenCollectiveAddress;
-
-    /// @notice A bool to prevent external eth from being received and locked in the contract
-    bool private _ethTransferable = false;
-
     /// @dev This empty reserved space is put in place to allow future versions to add new
     /// variables without shifting storage.
     uint256[500] private __gap;
@@ -90,190 +69,25 @@ contract NiftyApes is
     ///         Nifty Apes is intended to be deployed behind a proxy amd thus needs to initialize
     ///         its state outsize of a constructor.
     function initialize() public initializer {
-        EIP712Upgradeable.__EIP712_init("NiftyApes_Lending", "0.0.1");
-
         protocolInterestBps = 0;
         refinancePremiumLenderBps = 50;
         refinancePremiumProtocolBps = 0;
-        regenCollectiveBpsOfRevenue = 100;
-        regenCollectiveAddress = address(0x252de94Ae0F07fb19112297F299f8c9Cc10E28a6);
-
+    
         OwnableUpgradeable.__Ownable_init();
         PausableUpgradeable.__Pausable_init();
         ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
     }
 
-    /// @inheritdoc INiftyApesAdmin
-    function setCAssetAddress(address asset, address cAsset) external onlyOwner {
-        assetToCAsset[asset] = cAsset;
-        _cAssetToAsset[cAsset] = asset;
-
-        emit NewAssetListed(asset, cAsset);
-    }
-
-    /// @inheritdoc INiftyApesAdmin
-    function setMaxCAssetBalance(address asset, uint256 maxBalance) external onlyOwner {
-        maxBalanceByCAsset[getCAsset(asset)] = maxBalance;
-    }
-
-    /// @inheritdoc INiftyApesAdmin
+    /// @inheritdoc ILendingAdmin
     function pause() external onlyOwner {
         _pause();
     }
 
-    /// @inheritdoc INiftyApesAdmin
+    /// @inheritdoc ILendingAdmin
     function unpause() external onlyOwner {
         _unpause();
     }
 
-    /// @inheritdoc ILiquidity
-    function getCAssetBalance(address account, address cAsset) public view returns (uint256) {
-        return _balanceByAccountByAsset[account][cAsset].cAssetBalance;
-    }
-
-    /// @inheritdoc ILiquidity
-    function supplyErc20(address asset, uint256 tokenAmount)
-        external
-        whenNotPaused
-        nonReentrant
-        returns (uint256)
-    {
-        address cAsset = getCAsset(asset);
-
-        requireIsNotSanctioned(msg.sender);
-
-        uint256 cTokensMinted = mintCErc20(msg.sender, address(this), asset, tokenAmount);
-
-        _balanceByAccountByAsset[msg.sender][cAsset].cAssetBalance += cTokensMinted;
-
-        requireMaxCAssetBalance(cAsset);
-
-        emit Erc20Supplied(msg.sender, asset, tokenAmount, cTokensMinted);
-
-        return cTokensMinted;
-    }
-
-    /// @inheritdoc ILiquidity
-    function supplyCErc20(address cAsset, uint256 cTokenAmount)
-        external
-        whenNotPaused
-        nonReentrant
-    {
-        getAsset(cAsset); // Ensures asset / cAsset is in the allow list
-        IERC20Upgradeable cToken = IERC20Upgradeable(cAsset);
-
-        requireIsNotSanctioned(msg.sender);
-
-        cToken.safeTransferFrom(msg.sender, address(this), cTokenAmount);
-
-        _balanceByAccountByAsset[msg.sender][cAsset].cAssetBalance += cTokenAmount;
-
-        requireMaxCAssetBalance(cAsset);
-
-        emit CErc20Supplied(msg.sender, cAsset, cTokenAmount);
-    }
-
-    /// @inheritdoc ILiquidity
-    function withdrawErc20(address asset, uint256 tokenAmount)
-        public
-        whenNotPaused
-        nonReentrant
-        returns (uint256)
-    {
-        address cAsset = getCAsset(asset);
-        IERC20Upgradeable underlying = IERC20Upgradeable(asset);
-
-        if (msg.sender == owner()) {
-            uint256 cTokensBurnt = ownerWithdraw(asset, cAsset);
-            return cTokensBurnt;
-        } else {
-            uint256 cTokensBurnt = burnCErc20(asset, tokenAmount);
-
-            withdrawCBalance(msg.sender, cAsset, cTokensBurnt);
-
-            underlying.safeTransfer(msg.sender, tokenAmount);
-
-            emit Erc20Withdrawn(msg.sender, asset, tokenAmount, cTokensBurnt);
-
-            return cTokensBurnt;
-        }
-    }
-
-    /// @inheritdoc ILiquidity
-    function withdrawCErc20(address cAsset, uint256 cTokenAmount)
-        external
-        whenNotPaused
-        nonReentrant
-    {
-        // Making sure a mapping for cAsset exists
-        getAsset(cAsset);
-        IERC20Upgradeable cToken = IERC20Upgradeable(cAsset);
-
-        withdrawCBalance(msg.sender, cAsset, cTokenAmount);
-
-        cToken.safeTransfer(msg.sender, cTokenAmount);
-
-        emit CErc20Withdrawn(msg.sender, cAsset, cTokenAmount);
-    }
-
-    /// @inheritdoc ILiquidity
-    function supplyEth() external payable whenNotPaused nonReentrant returns (uint256) {
-        address cAsset = getCAsset(ETH_ADDRESS);
-
-        requireIsNotSanctioned(msg.sender);
-
-        uint256 cTokensMinted = mintCEth(msg.value);
-
-        _balanceByAccountByAsset[msg.sender][cAsset].cAssetBalance += cTokensMinted;
-
-        requireMaxCAssetBalance(cAsset);
-
-        emit EthSupplied(msg.sender, msg.value, cTokensMinted);
-
-        return cTokensMinted;
-    }
-
-    /// @inheritdoc ILiquidity
-    function withdrawEth(uint256 amount) external whenNotPaused nonReentrant returns (uint256) {
-        address cAsset = getCAsset(ETH_ADDRESS);
-
-        uint256 cTokensBurnt = burnCErc20(ETH_ADDRESS, amount);
-
-        withdrawCBalance(msg.sender, cAsset, cTokensBurnt);
-
-        payable(msg.sender).sendValue(amount);
-
-        emit EthWithdrawn(msg.sender, amount, cTokensBurnt);
-
-        return cTokensBurnt;
-    }
-
-    function ownerWithdraw(address asset, address cAsset) internal returns (uint256 cTokensBurnt) {
-        IERC20Upgradeable underlying = IERC20Upgradeable(asset);
-        uint256 ownerBalance = getCAssetBalance(owner(), cAsset);
-
-        uint256 ownerBalanceUnderlying = cAssetAmountToAssetAmount(cAsset, ownerBalance);
-
-        cTokensBurnt = burnCErc20(asset, ownerBalanceUnderlying);
-
-        uint256 bpsForRegen = (cTokensBurnt * regenCollectiveBpsOfRevenue) / MAX_BPS;
-
-        uint256 ownerBalanceMinusRegen = cTokensBurnt - bpsForRegen;
-
-        uint256 ownerAmountUnderlying = cAssetAmountToAssetAmount(cAsset, ownerBalanceMinusRegen);
-
-        uint256 regenAmountUnderlying = cAssetAmountToAssetAmount(cAsset, bpsForRegen);
-
-        withdrawCBalance(owner(), cAsset, cTokensBurnt);
-
-        underlying.safeTransfer(owner(), ownerAmountUnderlying);
-
-        underlying.safeTransfer(regenCollectiveAddress, regenAmountUnderlying);
-
-        emit PercentForRegen(regenCollectiveAddress, asset, regenAmountUnderlying, bpsForRegen);
-
-        emit Erc20Withdrawn(owner(), asset, ownerAmountUnderlying, ownerBalanceMinusRegen);
-    }
 
     /// @inheritdoc ILending
     function getLoanAuction(address nftContractAddress, uint256 nftId)
@@ -881,48 +695,24 @@ contract NiftyApes is
             SafeCastUpgradeable.toUint96(duration);
     }
 
-    /// @inheritdoc INiftyApesAdmin
+    /// @inheritdoc ILendingAdmin
     function updateProtocolInterestBps(uint96 newProtocolInterestBps) external onlyOwner {
         emit ProtocolInterestBpsUpdated(protocolInterestBps, newProtocolInterestBps);
         protocolInterestBps = newProtocolInterestBps;
     }
 
-    /// @inheritdoc INiftyApesAdmin
+    /// @inheritdoc ILendingAdmin
     function updateRefinancePremiumLenderBps(uint16 newPremiumLenderBps) external onlyOwner {
         require(newPremiumLenderBps <= MAX_FEE, "max fee");
         emit RefinancePremiumLenderBpsUpdated(refinancePremiumLenderBps, newPremiumLenderBps);
         refinancePremiumLenderBps = newPremiumLenderBps;
     }
 
-    /// @inheritdoc INiftyApesAdmin
+    /// @inheritdoc ILendingAdmin
     function updateRefinancePremiumProtocolBps(uint16 newPremiumProtocolBps) external onlyOwner {
         require(newPremiumProtocolBps <= MAX_FEE, "max fee");
         emit RefinancePremiumProtocolBpsUpdated(refinancePremiumProtocolBps, newPremiumProtocolBps);
         refinancePremiumProtocolBps = newPremiumProtocolBps;
-    }
-
-    /// @inheritdoc INiftyApesAdmin
-    function updateRegenCollectiveBpsOfRevenue(uint16 newRegenCollectiveBpsOfRevenue)
-        external
-        onlyOwner
-    {
-        require(newRegenCollectiveBpsOfRevenue <= MAX_FEE, "max fee");
-        require(newRegenCollectiveBpsOfRevenue >= regenCollectiveBpsOfRevenue, "must be greater");
-        emit RegenCollectiveBpsOfRevenueUpdated(
-            regenCollectiveBpsOfRevenue,
-            newRegenCollectiveBpsOfRevenue
-        );
-        regenCollectiveBpsOfRevenue = newRegenCollectiveBpsOfRevenue;
-    }
-
-    /// @inheritdoc INiftyApesAdmin
-    function updateRegenCollectiveAddress(address newRegenCollectiveAddress) external onlyOwner {
-        emit RegenCollectiveAddressUpdated(newRegenCollectiveAddress);
-        regenCollectiveAddress = newRegenCollectiveAddress;
-    }
-
-    function requireEthTransferable() internal view {
-        require(_ethTransferable, "eth not transferable");
     }
 
     function requireSignature65(bytes memory signature) internal pure {
@@ -1026,20 +816,8 @@ contract NiftyApes is
         require(creator == offer.creator, "offer creator mismatch");
     }
 
-    function requireSigner(address signer, address expected) internal pure {
-        require(signer == expected, "signer");
-    }
-
     function requireOfferCreator(address signer, address expected) internal pure {
         require(signer == expected, "offer creator");
-    }
-
-    function requireCAssetBalance(
-        address account,
-        address cAsset,
-        uint256 amount
-    ) internal view {
-        require(getCAssetBalance(account, cAsset) >= amount, "Insufficient cToken balance");
     }
 
     function requireOfferParity(LoanAuction storage loanAuction, Offer memory offer) internal view {
@@ -1113,18 +891,6 @@ contract NiftyApes is
         IERC721Upgradeable(nftContractAddress).safeTransferFrom(from, to, nftId);
     }
 
-    function sendValue(
-        address asset,
-        uint256 amount,
-        address to
-    ) internal {
-        if (asset == ETH_ADDRESS) {
-            payable(to).sendValue(amount);
-        } else {
-            IERC20Upgradeable(asset).safeTransfer(to, amount);
-        }
-    }
-
     function payoutCTokenBalances(
         LoanAuction storage loanAuction,
         address cAsset,
@@ -1148,93 +914,6 @@ contract NiftyApes is
     // solhint-disable-next-line no-empty-blocks
     receive() external payable {
         require(_ethTransferable, "eth not transferable");
-    }
-
-    function requireMaxCAssetBalance(address cAsset) internal view {
-        uint256 maxCAssetBalance = maxBalanceByCAsset[cAsset];
-        if (maxCAssetBalance != 0) {
-            require(maxCAssetBalance >= ICERC20(cAsset).balanceOf(address(this)), "max casset");
-        }
-    }
-
-    function mintCErc20(
-        address from,
-        address to,
-        address asset,
-        uint256 amount
-    ) internal returns (uint256) {
-        address cAsset = assetToCAsset[asset];
-        IERC20Upgradeable underlying = IERC20Upgradeable(asset);
-        ICERC20 cToken = ICERC20(cAsset);
-
-        underlying.safeTransferFrom(from, to, amount);
-        underlying.safeIncreaseAllowance(cAsset, amount);
-
-        uint256 cTokenBalanceBefore = cToken.balanceOf(address(this));
-        require(cToken.mint(amount) == 0, "cToken mint");
-        uint256 cTokenBalanceAfter = cToken.balanceOf(address(this));
-        return cTokenBalanceAfter - cTokenBalanceBefore;
-    }
-
-    function mintCEth(uint256 amount) internal returns (uint256) {
-        address cAsset = assetToCAsset[ETH_ADDRESS];
-        ICEther cToken = ICEther(cAsset);
-        uint256 cTokenBalanceBefore = cToken.balanceOf(address(this));
-        cToken.mint{ value: amount }();
-        uint256 cTokenBalanceAfter = cToken.balanceOf(address(this));
-        return cTokenBalanceAfter - cTokenBalanceBefore;
-    }
-
-    // @notice param amount is demoninated in the underlying asset, not cAsset
-    function burnCErc20(address asset, uint256 amount) internal returns (uint256) {
-        address cAsset = assetToCAsset[asset];
-        ICERC20 cToken = ICERC20(cAsset);
-
-        uint256 cTokenBalanceBefore = cToken.balanceOf(address(this));
-        _ethTransferable = true;
-        require(cToken.redeemUnderlying(amount) == 0, "redeemUnderlying failed");
-        _ethTransferable = false;
-        uint256 cTokenBalanceAfter = cToken.balanceOf(address(this));
-        return cTokenBalanceBefore - cTokenBalanceAfter;
-    }
-
-    /// @inheritdoc ILiquidity
-    function assetAmountToCAssetAmount(address asset, uint256 amount) public returns (uint256) {
-        address cAsset = assetToCAsset[asset];
-        ICERC20 cToken = ICERC20(cAsset);
-
-        uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
-        return Math.divScalarByExpTruncate(amount, exchangeRateMantissa);
-    }
-
-    function cAssetAmountToAssetAmount(address cAsset, uint256 amount) internal returns (uint256) {
-        ICERC20 cToken = ICERC20(cAsset);
-
-        uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
-        return Math.mulScalarTruncate(amount, exchangeRateMantissa);
-    }
-
-    function getCAsset(address asset) public view returns (address) {
-        address cAsset = assetToCAsset[asset];
-        require(cAsset != address(0), "asset allow list");
-        require(asset == _cAssetToAsset[cAsset], "non matching allow list");
-        return cAsset;
-    }
-
-    function getAsset(address cAsset) internal view returns (address) {
-        address asset = _cAssetToAsset[cAsset];
-        require(asset != address(0), "cAsset allow list");
-        require(cAsset == assetToCAsset[asset], "non matching allow list");
-        return asset;
-    }
-
-    function withdrawCBalance(
-        address account,
-        address cAsset,
-        uint256 cTokenAmount
-    ) internal {
-        requireCAssetBalance(account, cAsset, cTokenAmount);
-        _balanceByAccountByAsset[account][cAsset].cAssetBalance -= cTokenAmount;
     }
 
     function renounceOwnership() public override onlyOwner {}
