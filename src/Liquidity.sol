6d58cc7f88c7175a9b973f22792ae9db93824fb4
captnseagraves <k.s.seagraves@gmail.com>
2022-06-03 21:24:43 -0600

    add allowance check

--- src/Liquidity.sol
+++ src/Liquidity.sol
@@ -403,6 +403,10 @@ contract NiftyApesLiquidity is
         ICERC20 cToken = ICERC20(cAsset);
 
         underlying.safeTransferFrom(from, to, amount);
+        uint256 allowance = underlying.allowance(address(this), address(cToken));
+        if (allowance > 0) {
+            underlying.safeDecreaseAllowance(cAsset, allowance);
+        }
         underlying.safeIncreaseAllowance(cAsset, amount);
 
         uint256 cTokenBalanceBefore = cToken.balanceOf(address(this));
