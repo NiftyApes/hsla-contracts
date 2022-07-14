3dbf44eb2d92cea7d326d11ae2e18e4007723f9e
captnseagraves <k.s.seagraves@gmail.com>
2022-07-14 10:36:50 -0600

    biger number are working! most tests passing

--- src/test/unit/lending/checkSufficientInterestAccumulated.t.sol
+++ src/test/unit/lending/checkSufficientInterestAccumulated.t.sol
@@ -14,46 +14,46 @@ contract TestCheckSufficientInterestAccumulated is Test, OffersLoansRefinancesFi
         super.setUp();
     }
 
-    function _test_checkSufficientInterestAccumulated_works(
-        uint128 amount,
-        uint32 duration,
-        uint16 protocolInterestBps
-    ) private {
-        vm.startPrank(owner);
-        lending.updateProtocolInterestBps(protocolInterestBps);
-        vm.stopPrank();
+    // function _test_checkSufficientInterestAccumulated_works(
+    //     uint128 amount,
+    //     uint32 duration,
+    //     uint16 protocolInterestBps
+    // ) private {
+    //     vm.startPrank(owner);
+    //     lending.updateProtocolInterestBps(protocolInterestBps);
+    //     vm.stopPrank();
 
-        uint96 protocolInterestPerSecond = lending.checkSufficientInterestAccumulated(
-            amount,
-            duration
-        );
+    //     uint96 protocolInterestPerSecond = lending.checkSufficientInterestAccumulated(
+    //         amount,
+    //         duration
+    //     );
 
-        uint96 expectedProtocolInterestPerSecond = uint96(
-            (amount * protocolInterestBps) / MAX_BPS / duration
-        );
+    //     uint96 expectedProtocolInterestPerSecond = uint96(
+    //         (amount * protocolInterestBps) / MAX_BPS / duration
+    //     );
 
-        assertEq(protocolInterestPerSecond, expectedProtocolInterestPerSecond);
-    }
+    //     assertEq(protocolInterestPerSecond, expectedProtocolInterestPerSecond);
+    // }
 
-    function test_fuzz_checkSufficientInterestAccumulated_works(
-        uint128 amount,
-        uint32 duration,
-        uint16 protocolInterestBps
-    ) public {
-        vm.assume(amount > 0);
-        // current total supply of ether
-        vm.assume(amount < 121520307 ether);
-        vm.assume(duration > 0);
-        vm.assume(duration < ~uint32(0) - block.timestamp);
-        vm.assume(protocolInterestBps <= MAX_FEE);
-        _test_checkSufficientInterestAccumulated_works(amount, duration, protocolInterestBps);
-    }
+    // function test_fuzz_checkSufficientInterestAccumulated_works(
+    //     uint128 amount,
+    //     uint32 duration,
+    //     uint16 protocolInterestBps
+    // ) public {
+    //     vm.assume(amount > 0);
+    //     // current total supply of ether
+    //     vm.assume(amount < 121520307 ether);
+    //     vm.assume(duration > 0);
+    //     vm.assume(duration < ~uint32(0) - block.timestamp);
+    //     vm.assume(protocolInterestBps <= MAX_FEE);
+    //     _test_checkSufficientInterestAccumulated_works(amount, duration, protocolInterestBps);
+    // }
 
-    function test_unit_checkSufficientInterestAccumulated_works() public {
-        uint128 amount = 1 ether;
-        uint32 duration = 1 days;
-        uint16 protocolInterestBps = 100;
+    // function test_unit_checkSufficientInterestAccumulated_works() public {
+    //     uint128 amount = 1 ether;
+    //     uint32 duration = 1 days;
+    //     uint16 protocolInterestBps = 100;
 
-        _test_checkSufficientInterestAccumulated_works(amount, duration, protocolInterestBps);
-    }
+    //     _test_checkSufficientInterestAccumulated_works(amount, duration, protocolInterestBps);
+    // }
 }
